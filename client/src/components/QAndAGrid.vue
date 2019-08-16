a70f6dd973222170ce93ec779d1a2bf2aae55d38
Fidelma <fidelma.beagan@gmail.com>
2019-08-16 16:53:10 +0100

    Created li for random question

---
+++ client/src/components/QAndAGrid.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <div class="">
+
+  <!-- <div class="" v-for="question in questions"> -->
+  <ul>
+    <li>{{questions[randomIndex].question}}</li>
+    <li>{{questions[randomIndex].answer}}</li>
+  </ul>
+
+  </div>
+<!-- </div> -->
+
+</template>
+
+<script>
+export default {
+  name: 'qanda-grid',
+  props: ['questions', 'randomIndex']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
