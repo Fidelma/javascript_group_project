73eea637a3225c3df2474ea59f4e322d3baa50ee
mikefallon <mikefallon81@gmail.com>
2019-08-17 22:01:26 +0100

    styling added

--- client/src/components/QAndAGrid.vue
+++ client/src/components/QAndAGrid.vue
@@ -1,11 +1,11 @@
 <template lang="html">
-  <div class="">
+  <div class="question-grid">
 
 
   <h3>{{questions[randomIndex].question}}</h3>
-  <ul v-for="answer in this.randomList">
-    <li>{{answer.body}}</li>
-  </ul>
+  <div class="question">
+      <button class="" v-for="answer in this.randomList">{{answer.body}}</button>
+  </div>
   <!-- <p>{{questions[0].answerObject}}</p> -->
 
   </div>
@@ -22,4 +22,26 @@ export default {
 </script>
 
 <style lang="css" scoped>
+
+.question {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  padding: 15px 0px 15px 0px;
+
+}
+
+.question-grid {
+  border-style: double;
+  margin: 30px 0px 30px 0px;
+  padding: 15px 0px 15px 0px;
+
+
+}
+
+button {
+  padding: 10px;
+  font-size: 1em;
+}
+
 </style>
