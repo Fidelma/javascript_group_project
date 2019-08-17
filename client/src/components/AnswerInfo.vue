c0552508761371e3b97d49a567e73a175065a93c
mikefallon <mikefallon81@gmail.com>
2019-08-17 19:35:01 +0100

    added answer detail to component

--- client/src/components/AnswerInfo.vue
+++ client/src/components/AnswerInfo.vue
@@ -1,11 +1,30 @@
 <template lang="html">
-  <p>Hello</p>
+  <div class="">
+
+    <p>{{questions[randomIndex].answerBody.title}}</p>
+    <p>{{questions[randomIndex].answerBody.text}}</p>
+    <iframe v-bind:src="questions[randomIndex].answerBody.counter1URL" width="300" height="100"></iframe>
+    <img v-bind:src="questions[randomIndex].answerBody.image" alt="">
+
+  </div>
 </template>
+
+
 
 <script>
 export default {
+  name: "answer-info",
+  props: ["questions", "randomIndex"]
 }
 </script>
 
 <style lang="css" scoped>
+img {
+  width:300px;
+  height:250px;
+}
+
+iframe {
+  color:black;
+}
 </style>
