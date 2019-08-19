88de3cfcd56a44d72b44f5a8fbf029da540cac7d
Fidelma <fidelma.beagan@gmail.com>
2019-08-19 11:50:24 +0100

    setup Categories Grid component

---
+++ client/src/components/CategoriesGrid.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <ul>
+    <li v-for="category in categories">{{category.body}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'categories-grid',
+  props: ['categories']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
