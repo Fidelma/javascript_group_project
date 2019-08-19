37ab685412ca27b6ba30af2becdb83d2984bdb9a
mikefallon <mikefallon81@gmail.com>
2019-08-19 16:45:42 +0100

    logic added to checkAnswer method

--- client/src/components/QAndAGrid.vue
+++ client/src/components/QAndAGrid.vue
@@ -3,13 +3,12 @@
 
 
   <h3>{{questions[index].question}}</h3>
-  <div class="question">
-      <button @click="checkAnswer(index, answer)"class="" v-for="answer in randomList">{{answer.body}}</button>
-  </div>
-  <!-- <p>{{questions[0].answerObject}}</p> -->
-
+    <div class="question" >
+        <button id="questions" class="" @click="checkAnswer(index, answer)" v-for="answer in randomList">{{answer.body}}</button>
+    </div>
   </div>
 
+<!-- v-bind:class="[checkAnswer ? 'correct-answer' : '', 'incorrect-answer']" -->
 
 </template>
 
@@ -22,7 +21,12 @@ export default {
   methods: {
     checkAnswer(id, answer){
       if(answer.type){
-        eventBus.$emit('correct-answer', answer.type)
+        eventBus.$emit('correct-answer', answer.type);
+        const button = document.getElementById("question")
+        element.classList.add("correct-answer")
+      }else {
+        const button = document.getElementById("question")
+        element.classList.add("incorrect-answer")
       }
 
     }
@@ -52,6 +56,16 @@ export default {
 button {
   padding: 10px;
   font-size: 1em;
+}
+
+.correct-answer {
+  background-color: green;
+  font-weight: bold;
+}
+
+.incorrect-answer {
+  background-color: red;
+
 }
 
 </style>
