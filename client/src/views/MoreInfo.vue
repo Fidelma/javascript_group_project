ce9cd633daa458a976befbd57f87833e80249cc0
Fidelma <fidelma.beagan@gmail.com>
2019-08-19 11:51:47 +0100

    Created more-info view

---
+++ client/src/views/MoreInfo.vue
@@ -0,0 +1,41 @@
+<template lang="html">
+  <div class="">
+    <categories-grid :categories="categories"/>
+    <category-info :categories="categories"/>
+  </div>
+</template>
+
+<script>
+import CategoriesService from '@/services/CategoriesService.js'
+import CategoriesGrid from '@/components/CategoriesGrid.vue'
+import CategoryInfo from '@/components/CategoryInfo.vue'
+
+export default {
+  name: 'more-info',
+
+  data(){
+    return{
+      categories: []
+    }
+  },
+  components: {
+    'categories-grid': CategoriesGrid,
+    'category-info': CategoryInfo
+  },
+
+  mounted() {
+    this.fetchCategories()
+  },
+
+  methods: {
+    fetchCategories(){
+      CategoriesService.getCategories()
+        .then(categories => this.categories = categories);
+    }
+  }
+
+}
+</script>
+
+<style lang="css" scoped>
+</style>
