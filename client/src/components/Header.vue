036d1a7c891073bb58b85ed7df2dabdd36475b4f
mikefallon <mikefallon81@gmail.com>
2019-08-19 21:44:36 +0100

    header component added

---
+++ client/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div class="">
+    <h1>We're Screwed!</h1>
+    <img src="../../public/header_image.jpg" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header'
+}
+</script>
+
+<style lang="css" scoped>
+h1 {
+  height: 200px;
+}
+</style>
