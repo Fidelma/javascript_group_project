0caf5f160a76d6c807f97b82b00d4921cb213c1e
Rana-akbar <rana.akbar.08@gmail.com>
2019-08-19 15:19:04 +0100

    Added Burger Nav bar via w3 Schools

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,32 @@
 <template>
   <div id="app">
-    <nav>
-    <router-link :to="{ name: 'home'}">Home</router-link>
-    
-    <router-link :to="{ name: 'moreinfo'}">MoreInfo</router-link>
-  </nav>
-<router-view id="view"></router-view>
 
+    <!-- sidebar -->
+    <div class="w3-sidebar w3-bar-block w3-card w3-animate-left" style="display:none" id="mySidebar">
+      <button class="w3-bar-item w3-button w3-large"
+      @click="w3_close()">Close &times;</button>
+      <router-link :to="{ name: 'home'}">Home</router-link>
+      <br>
+      <router-link :to="{ name: 'moreinfo'}">MoreInfo</router-link>
+    </div>
+    <div class="w3-main" style="margin-left:200px">
   </div>
+
+    <div id="main">
+      <div class="w3-teal">
+        <button id="openNav" class="w3-button w3-teal w3-xlarge" @click="w3_open()">&#9776; </button>
+        <h1>CLIMATE QUIZ</h1>
+        <div class="w3-container">
+        </div>
+      </div>
+      <!-- sidebar above  -->
+
+      <router-view id="view"></router-view>
+
+
+    </div>
+  </div>
+
 </template>
 
 <script>
@@ -17,16 +36,30 @@ import { eventBus } from '@/main.js'
 export default {
   name: 'app',
 
+  methods: {
+    w3_open() {
+      document.getElementById("main").style.marginLeft = "25%";
+      document.getElementById("mySidebar").style.width = "25%";
+      document.getElementById("mySidebar").style.display = "block";
+      document.getElementById("openNav").style.display = 'none';
+    },
+    w3_close() {
+      document.getElementById("main").style.marginLeft = "0%";
+      document.getElementById("mySidebar").style.display = "none";
+      document.getElementById("openNav").style.display = "inline-block";
+    }
+  }
 }
 </script>
 
 <style>
+
+
 #app {
+  margin-top: 0px;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
