2f9122f806b041f5e18d7da6cd7ba79cee669f97
Fidelma <fidelma.beagan@gmail.com>
2019-08-19 11:09:52 +0100

    added home router

---
+++ client/src/router.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from '@/views/Home.vue';
+// import MoreInfo from '@/components/MoreInfo';
+Vue.use(Router)
+const router = new Router({
+  routes: [
+    {
+      path: '',
+      name: 'home',
+      component: Home
+    }
+    // ,
+    // {
+    //   path: '/moreinfo',
+    //   name: 'moreinfo',
+    //   component: MoreInfo,
+    // }
+  ]
+})
+export default router;
