244cad9897ed829f9f8e417b47cfc34a7965f0c9
Fidelma <fidelma.beagan@gmail.com>
2019-08-19 11:51:01 +0100

    Created More-info path

--- client/src/router.js
+++ client/src/router.js
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/views/Home.vue';
-// import MoreInfo from '@/components/MoreInfo';
+import MoreInfo from '@/views/MoreInfo';
+
 Vue.use(Router)
 const router = new Router({
   routes: [
@@ -9,13 +10,12 @@ const router = new Router({
       path: '',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/moreinfo',
+      name: 'moreinfo',
+      component: MoreInfo,
     }
-    // ,
-    // {
-    //   path: '/moreinfo',
-    //   name: 'moreinfo',
-    //   component: MoreInfo,
-    // }
   ]
 })
 export default router;
