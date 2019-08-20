95981f936d8fd0e168ab01f0769cce1f03bb72e4
mikefallon <mikefallon81@gmail.com>
2019-08-20 20:06:59 +0100

    eventbus and data added

---
+++ client/src/components/AddQuestionForm.vue
@@ -0,0 +1,54 @@
+<template lang="html">
+
+</template>
+
+<script>
+
+import { eventBus } from '@/main';
+import QuestionsService from '@/services/QuestionsService';
+
+
+export default {
+  name: "questions-form",
+  data(){
+    return {
+      title: "",
+      question: "",
+      answerObject: [{
+        correct: null,
+        body: ""
+      },
+      {
+        correct: null,
+        body: ""
+      },
+      {
+        correct: null,
+        body: ""
+      }],
+      categories: [],
+      answerBody: {
+        title: "",
+        facts: [{
+          fact: "one",
+          body: ""
+        },
+        {
+          fact: "two",
+          body: ""
+        },
+        {
+          fact: "three",
+          body: ""
+        }],
+        counter1URL: "",
+        image: "",
+        imageDetail: ""
+      }
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
