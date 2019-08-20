c7c12f2b1903347dbec5db6d9292aac1273ae1ef
mikefallon <mikefallon81@gmail.com>
2019-08-20 11:10:30 +0100

    Added counters to categories

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -140,8 +140,9 @@ db.categories.insertMany([
       }
     ],
    data: {},
-   counters: ['counter1', 'counter2']
-  },
+   counterOneSrc: "<iframe height='200' src='https://www.theworldcounts.com/counters/degradation_and_destruction_of_ecosystems/facts_on_deforestation/embed?background_color=white&color=black&font_family=%22Helvetica+Neue%22%2C+Arial%2C+sans-serif&font_size=28&token=da6cee8234591f3a2542678e4c4510fb48b62b69' style='border: none' width='500'></iframe>",
+   counterTwoSrc: "<iframe height='200' src='https://www.theworldcounts.com/counters/degradation_and_destruction_of_ecosystems/forestation_and_deforestation/embed?background_color=white&color=black&font_family=%22Helvetica+Neue%22%2C+Arial%2C+sans-serif&font_size=28&token=da6cee8234591f3a2542678e4c4510fb48b62b69' style='border: none' width='500'></iframe>"
+},
   {
     title: "Diet",
     text: [
@@ -159,6 +160,7 @@ db.categories.insertMany([
       }
     ],
    data: {},
-   counters: ['counter1', 'counter2']
+   counterOneSrc: "<iframe height='200' src='https://www.theworldcounts.com/counters/world_food_consumption_statistics/environmental_effects_of_meat_production/embed?background_color=white&color=black&font_family=%22Helvetica+Neue%22%2C+Arial%2C+sans-serif&font_size=28&token=da6cee8234591f3a2542678e4c4510fb48b62b69' style='border: none' width='500'></iframe>",
+   counterTwoSrc: "<iframe height='200' src='https://www.theworldcounts.com/counters/world_food_consumption_statistics/world_food_waste_statistics/embed?background_color=white&color=black&font_family=%22Helvetica+Neue%22%2C+Arial%2C+sans-serif&font_size=28&token=da6cee8234591f3a2542678e4c4510fb48b62b69' style='border: none' width='500'></iframe>"
  }
 ])
