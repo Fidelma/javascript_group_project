ce8812fed12aea561032b75ae0611e44c4c7ec5e
mikefallon <mikefallon81@gmail.com>
2019-08-20 19:21:32 +0100

    Admin route added

--- client/src/router.js
+++ client/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/views/Home.vue';
 import MoreInfo from '@/views/MoreInfo';
+import Admin from '@/views/Admin'
 
 Vue.use(Router)
 const router = new Router({
@@ -15,6 +16,11 @@ const router = new Router({
       path: '/moreinfo',
       name: 'moreinfo',
       component: MoreInfo,
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: Admin,
     }
   ]
 })
